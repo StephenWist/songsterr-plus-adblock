dabcbd451ab79f95ddc4fd445114f42860888ab1
Stephen Wist <stphnwist@gmail.com>
2024-05-15 13:48:39 -0400

    Add manifest and ad removing JS.

---
+++ songsterr-plus-adblock.js
@@ -0,0 +1,4 @@
+// if we get the 'showroom' section songsterr seems to be able to detect this and recreate it. So we get its child div. Murder the child.
+document.getElementById('showroom').firstChild.remove();
+
+document.getElementById('promo').remove();
